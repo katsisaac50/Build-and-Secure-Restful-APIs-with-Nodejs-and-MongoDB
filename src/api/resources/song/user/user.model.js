940fa6d1ef4d854d78b9254a1fbd4b4d6112bc5f
katongole <katsisaac50@gmail.com>
2023-03-16 13:27:40 +0100

    create user models

---
+++ src/api/resources/song/user/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import mongoosePaginate from'mongoose-paginate';
+
+
+const {Schema} = mongoose;
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required:true
+  },
+  lastName: {
+    type: String,
+    required:true
+  },
+  email: {
+    type: String,
+    required:true,
+    unique:true,
+  },
+  password: {
+    type: String,
+    required:true
+  },
+  
+});
+
+
+export default mongoose.model('User',userSchema);
